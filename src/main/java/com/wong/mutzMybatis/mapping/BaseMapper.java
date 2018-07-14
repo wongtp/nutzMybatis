fd25b9b0690f5ea26fac173ec56b50059e3bfa1e
黄小天\wong <1853955116@qq.com>
2018-07-14 18:01:27 +0800

    解决编译后 baseMapper 找不到参数的问题

--- src/main/java/com/wong/mutzMybatis/mapping/BaseMapper.java
+++ src/main/java/com/wong/mutzMybatis/mapping/BaseMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Options.FlushCachePolicy;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultType;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -22,7 +23,10 @@ public interface BaseMapper {
 	@Select("select * from ${tableName} where 1 = 1 and ${primaryKeyName} = #{primaryKeyValue}")
 	@Options(useCache = true, flushCache = FlushCachePolicy.FALSE, timeout = 5000)
 	@ResultType(DataRecord.class)
-	DataRecord getDataRecord(String tableName, String primaryKeyName, Object primaryKeyValue) throws Exception;
+	DataRecord getDataRecord(
+			@Param("tableName")String tableName, 
+			@Param("primaryKeyName")String primaryKeyName, 
+			@Param("primaryKeyValue")Object primaryKeyValue) throws Exception;
 	
 	//@SelectProvider(type = BaseMapperSqlProvider.class, method="getTableListSql")
 	@Select({"<script>",
@@ -36,7 +40,9 @@ public interface BaseMapper {
 			"</script>"})
 	@Options(useCache = true, flushCache = FlushCachePolicy.FALSE, timeout = 5000)
 	@ResultType(DataRecord.class)
-	List<DataRecord> getTableList(String tableName, Object parameters) throws Exception;
+	List<DataRecord> getTableList(
+			@Param("tableName")String tableName, 
+			@Param("parameters")Object parameters) throws Exception;
 	
 	@Select({"<script>",
 			"SELECT count(0) FROM ${tableName} t where 1 = 1",
@@ -46,13 +52,21 @@ public interface BaseMapper {
 			"		</foreach>",
 			"	</if>",
 			"</script>"})
-	Long getCount(String tableName, Object parameters) throws Exception;
+	Long getCount(
+			@Param("tableName")String tableName, 
+			@Param("parameters")Object parameters) throws Exception;
 	
 	@Delete("delete from ${tableName} where 1 = 1 and ${primaryKey} = #{primaryKeyValue}")
-	Integer delete(String tableName, String primaryKey, Object primaryKeyValue) throws Exception;
+	Integer delete(
+			@Param("tableName")String tableName, 
+			@Param("primaryKey")String primaryKey, 
+			@Param("primaryKeyValue")Object primaryKeyValue) throws Exception;
 	
 	@Delete("delete from ${tableName} where 1 = 1 and ${primaryKey} in (${primaryKeyValues})")
-	Integer deleteByIds(String tableName, String primaryKey, String primaryKeyValues) throws Exception;
+	Integer deleteByIds(
+			@Param("tableName")String tableName, 
+			@Param("primaryKey")String primaryKey, 
+			@Param("primaryKeyValues")String primaryKeyValues) throws Exception;
 	
 	@Delete({
 		"<script>",
@@ -65,13 +79,15 @@ public interface BaseMapper {
 		"	</if>",
 		"</if>",
 		"</script>"})
-	Integer deleteByParam(String tableName, Object parameters) throws Exception;
+	Integer deleteByParam(
+			@Param("tableName")String tableName, 
+			@Param("parameters")Object parameters) throws Exception;
 	
 	@Update("<script>update ${tableName} SET ${values} <if test=\"where != null and where != ''\"> where ${where} </if></script>")
-	Integer update(Object parameters) throws Exception;
+	Integer update(@Param("parameters")Object parameters) throws Exception;
 	
 	@Insert("insert into ${tableName}(${columns}) values(${values})")
-	Integer insert(Object parameters) throws Exception;
+	Integer insert(@Param("parameters")Object parameters) throws Exception;
 	
 	@Insert({
 		"<script>",
@@ -82,6 +98,6 @@ public interface BaseMapper {
 		"		</foreach>" + 
 		"	</if>",
 		"</script>"})
-	Integer insertList(Object parameters) throws Exception;
+	Integer insertList(@Param("parameters")Object parameters) throws Exception;
 	
 }
